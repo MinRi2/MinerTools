17e56a7db107dadec1daceb69a1e0a47d61fd3aa
rlzzj <2275045670@qq.com>
2022-03-20 21:20:19 +0800

    新增功能[E]: DraggableTable可回忆上次的位置

--- src/MinerTools/ui/tables/DraggableTable.java
+++ src/MinerTools/ui/tables/DraggableTable.java
@@ -4,25 +4,54 @@ import arc.input.*;
 import arc.math.geom.*;
 import arc.scene.*;
 import arc.scene.event.*;
+import arc.scene.ui.layout.*;
 import arc.util.*;
+
+import static MinerTools.MinerVars.mSettings;
 
 /** Table that can be dragged */
 public class DraggableTable extends BaseTable{
     public Element draggier;
     public boolean isDragging = false;
 
-    public DraggableTable(){
-        this(null);
+    private boolean savePos;
+
+    public DraggableTable(boolean savePos){
+        this("", savePos);
     }
 
-    public DraggableTable(Element draggier){
+    public DraggableTable(String name, boolean savePos){
+        this(name, null, savePos);
+    }
+
+    public DraggableTable(String name, Element draggier, boolean savePos){
+        this.name = name;
+        this.savePos = savePos;
+
         setDraggier(draggier);
+
+        if(savePos){
+            setLastPos();
+        }
+    }
+
+    public void setLastPos(){
+        if(name != null && !name.equals("")){
+            float x = mSettings.getFloat("ui." + name + ".pos" + ".x");
+            float y = mSettings.getFloat("ui." + name + ".pos" + ".y");
+            Log.info("MinerTools DraggableTable Read: " + "(" + x + "," + y + ")");
+            setPosition(x, y);
+            keepInStage();
+        }
     }
 
     public void setDraggier(Element draggier){
         Element lastDraggier = this.draggier;
         this.draggier = draggier;
-        if(lastDraggier != draggier) addListener();
+        if(lastDraggier != draggier){
+            if(lastDraggier != null) lastDraggier.getListeners().removeAll(l -> l instanceof DragListener);
+            addListener();
+        }
     }
 
     public void addListener(){
@@ -30,29 +59,42 @@ public class DraggableTable extends BaseTable{
             return;
         }
 
-        draggier.addListener(new InputListener(){
-            float fromx, fromy;
+        draggier.addListener(new DragListener(this));
+    }
 
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button){
-                fromx = x;
-                fromy = y;
-                isDragging = true;
-                return true;
+    public static class DragListener extends InputListener{
+        DraggableTable target;
+        float fromx, fromy;
+
+        public DragListener(DraggableTable target){
+            this.target = target;
+        }
+
+        @Override
+        public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button){
+            fromx = x;
+            fromy = y;
+            target.isDragging = true;
+            return true;
+        }
+
+        @Override
+        public void touchDragged(InputEvent event, float x, float y, int pointer){
+            Vec2 v = target.localToStageCoordinates(Tmp.v1.set(x, y));
+            target.setPosition(v.x - fromx, v.y - fromy);
+
+            target.pack();
+            target.keepInStage();
+
+            if(target.savePos){
+                mSettings.put("ui." + target.name + ".pos" + ".x", target.x);
+                mSettings.put("ui." + target.name + ".pos" + ".y", target.y);
             }
+        }
 
-            @Override
-            public void touchDragged(InputEvent event, float x, float y, int pointer){
-                Vec2 v = localToStageCoordinates(Tmp.v1.set(x, y));
-                setPosition(v.x - fromx, v.y - fromy);
-
-                keepInStage();
-            }
-
-            @Override
-            public void touchUp(InputEvent e, float x, float y, int pointer, KeyCode button){
-                isDragging = false;
-            }
-        });
+        @Override
+        public void touchUp(InputEvent e, float x, float y, int pointer, KeyCode button){
+            target.isDragging = false;
+        }
     }
 }
