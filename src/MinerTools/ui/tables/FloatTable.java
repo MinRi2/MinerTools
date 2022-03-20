ed8d93ee13678e2296100d919299148a5a95d7b2
rlzzj <2275045670@qq.com>
2022-03-20 21:20:57 +0800

    更新[?]: FloatTable.java

--- src/MinerTools/ui/tables/FloatTable.java
+++ src/MinerTools/ui/tables/FloatTable.java
@@ -2,32 +2,51 @@ package MinerTools.ui.tables;
 
 import arc.*;
 import arc.scene.ui.layout.*;
+import mindustry.game.*;
 import org.jetbrains.annotations.ApiStatus.*;
 
+import static MinerTools.ui.MStyles.floatb;
 import static arc.Core.scene;
+import static mindustry.Vars.state;
+import static mindustry.gen.Tex.*;
 import static mindustry.ui.Styles.*;
 
 public class FloatTable extends DraggableTable{
-    private String name;
-
     private Table title;
+    private Table cont;
 
     public FloatTable(String name){
-        this.name = name;
+        super(name, true);
+
+        setLastPos();
         setup();
+
+        Events.on(EventType.WorldLoadEvent.class, e -> {
+            addUI();
+            rebuildCont(cont);
+        });
+
+        update(this::update);
     }
 
     private void setup(){
         table(black3, t -> {
-            title = t;
-            setDraggier(t);
-            t.add(Core.bundle.get(name)).padLeft(3f).left();
-            t.button("X", () -> {});
-        }).growX();
+            setTitle(t);
+
+            t.add(Core.bundle.get(name)).padLeft(3f).growX().left();
+            t.button("x", floatb, () -> {
+//                TODO
+            }).width(35f).right();
+        }).fillX().minWidth(250f);
 
         row();
 
-        table(this::rebuildCont).fillX();
+        table(this::setCont).fillX().top().right();
+    }
+
+    @OverrideOnly
+    protected void rebuildCont(Table cont){
+        cont.clear();
     }
 
     @Override
@@ -35,6 +54,18 @@ public class FloatTable extends DraggableTable{
         scene.add(this);
     }
 
-    @OverrideOnly
-    private void rebuildCont(Table table){}
+    protected void update(){
+        if(state.isMenu()){
+            remove();
+        }
+    }
+
+    public void setTitle(Table title){
+        this.title = title;
+        setDraggier(title);
+    }
+
+    public void setCont(Table cont){
+        this.cont = cont;
+    }
 }
