accc577f7878febfacde3683911ad36300356647
rlzzj <2275045670@qq.com>
2022-04-04 21:48:43 +0800

    修复[C]: Alert判断错误

--- src/MinerTools/graphics/Drawer.java
+++ src/MinerTools/graphics/Drawer.java
@@ -60,7 +60,7 @@ public class Drawer{
                 data.buildings.getObjects(tmp);
                 for(Building building : tmp){
                     if(building instanceof TurretBuild turretBuild){
-                        if(!player.isNull() && mSettings.getBool("turretAlert")){
+                        if(!player.unit().isNull() && mSettings.getBool("turretAlert")){
                             turretAlert(turretBuild);
                         }
 
@@ -74,7 +74,7 @@ public class Drawer{
 
                 var cores = player.team().cores();
                 for(Unit unit : data.units){
-                    if(!player.isNull() && mSettings.getBool("unitAlert")){
+                    if(!player.unit().isNull() && mSettings.getBool("unitAlert")){
                         unitAlert(unit);
                     }
 
