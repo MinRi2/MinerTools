d1febaf7b013b640c837ee8bd351a3559b607258
rlzzj <2275045670@qq.com>
2022-05-31 12:56:41 +0800

    [Update]: Better cameraRender

--- src/MinerTools/graphics/renderer/BuildRender.java
+++ src/MinerTools/graphics/renderer/BuildRender.java
@@ -6,24 +6,10 @@ import arc.math.geom.*;
 import arc.struct.*;
 import arc.util.*;
 import mindustry.*;
-import mindustry.game.EventType.*;
 import mindustry.game.Teams.*;
 import mindustry.gen.*;
-import mindustry.world.*;
 
 public class BuildRender extends BaseRender<BuildDrawer<?>>{
-    /* Tiles for render in camera */
-    private static QuadTree<Tile> tiles;
-
-    static {
-        Events.on(WorldLoadEvent.class, e -> {
-            tiles = new QuadTree<>(Vars.world.getQuadBounds(Tmp.r1));
-
-            for(Tile tile : Vars.world.tiles){
-                tiles.insert(tile);
-            }
-        });
-    }
 
     @Override
     public void globalRender(Seq<BuildDrawer<?>> validDrawers){
@@ -40,14 +26,18 @@ public class BuildRender extends BaseRender<BuildDrawer<?>>{
 
     @Override
     public void cameraRender(Seq<BuildDrawer<?>> validDrawers){
-        tiles.intersect(Core.camera.bounds(Tmp.r1), tile -> {
-            Building building = tile.build;
+        Rect bounds = Core.camera.bounds(Tmp.r1);
 
-            if(building != null){
+        for(TeamData data : Vars.state.teams.getActive()){
+            var buildingTree = data.buildingTree;
+
+            if(buildingTree == null) continue;
+
+            buildingTree.intersect(bounds, build -> {
                 for(BuildDrawer<?> drawer : validDrawers){
-                    drawer.tryDraw(building);
+                    drawer.tryDraw(build);
                 }
-            }
-        });
+            });
+        }
     }
 }
