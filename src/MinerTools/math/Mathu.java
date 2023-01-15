d2718f4c8113192825d087e65dd60fa1a246bd6f
RICCJ <85016284+RICCJ@users.noreply.github.com>
2023-01-15 20:19:56 +0800

    错误索引

--- src/MinerTools/math/Mathu.java
+++ src/MinerTools/math/Mathu.java
@@ -11,7 +11,7 @@ public class Mathu{
             return out.set(points[0], points[1]);
         }else if(size == 1){
             Vec2 v1 = Tmp.v1.set(points[0], points[1]);
-            Vec2 v2 = Tmp.v2.set(points[3], points[4]);
+            Vec2 v2 = Tmp.v2.set(points[2], points[3]);
 
             return out.set(v1).mulAdd(v2.sub(v1), 0.5f);
         }else{
